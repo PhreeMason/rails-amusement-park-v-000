97ade5dbef3854751476f96759944a110add5347
PhreeMason <mr.omarmason@gmail.com>
2017-05-16 21:00:03 +0000

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,21 @@
+class SessionsController < ApplicationController
+  
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.find_by(name: params[:user][:name])
+    if @user
+      if @user.authenticate(params[:user][:password])
+        session[:user_id] = @user.id
+        redirect_to @user
+      end
+    else
+      flash[:alert] = "Something went wrong let's try that again."
+      redirect_to new_sessions_path
+    end
+  end
+  
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+    resource :sessions, only: :create
+    get '/signin' => 'sessions#new', as: :new_sessions
+    
     resource :users
+    root 'application#home'
+    
 end
+
+#rails s -p $PORT -b $IP
